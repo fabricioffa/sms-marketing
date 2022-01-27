ef6f0047ebe1e4155441e8ad4a39369aad2fec11
Fabricio de Freitas <fabricioffa08@gmail.com>
2022-01-27 19:06:15 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 })->name('homepage');
+
+Route::get('em-massa', function () {
+    return view('em-massa');
+})->name('em-massa');
+
+Route::get('para-empresa', function () {
+    return view('para-empresa');
+})->name('para-empresa');
+
+Route::get('rotas-sim', function () {
+    return view('rotas-sim');
+})->name('rotas-sim');
+
+Route::get('sem-restricoes', function () {
+    return view('sem-restricoes');
+})->name('sem-restricoes');
+
+Route::get('apis', function () {
+    return view('apis');
+})->name('apis');
+
+Route::get('precos', function () {
+    return view('precos');
+})->name('precos');
+
+Route::get('registro', function () {
+    return view('registro');
+})->name('registro');
