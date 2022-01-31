8aa46d762870f98b8ae147ba45ec11e9d48d985d
Fabricio de Freitas <fabricioffa08@gmail.com>
2022-01-31 18:11:15 -0300

    light-navbar

---
+++ resources/views/includes/light-navbar.blade.php
@@ -0,0 +1,42 @@
+<aside class="bg-white fixed-top d-flex border-bottom border-color-navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-dark col-lg-10 col-xl-9 bg-white ms-auto mx-lg-auto px-lg-5">
+        <div class="container-fluid py-1">
+            <a class="navbar-brand d-none d-lg-block" href="{{ route('homepage') }}"><img
+                    src="{{ asset('assets/imgs/logos/navbar.svg') }}" alt=""></a>
+            <button class="navbar-toggler d-block d-lg-none ms-auto ms-lg-0 " type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav gap-4 fw-500 align-items-baseline font-space-grotesk">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light-navbar" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            SMS Marketing
+                        </a>
+                        <ul class="dropdown-menu bg-white rounded-10" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item text-light-navbar" href="{{ route('em-massa') }}">SMS em Massa</a>
+                            </li>
+                            <li><a class="dropdown-item text-light-navbar" href="{{ route('para-empresa') }}">SMS massivos
+                                    para empresas</a></li>
+                            <li><a class="dropdown-item text-light-navbar" href="{{ route('rotas-sim') }}">SMS por Rotas
+                                    SIM</a></li>
+                            <li><a class="dropdown-item text-light-navbar" href="{{ route('sem-restricoes') }}">SMS sem
+                                    Restrições</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light-navbar" href="{{ route('apis') }}" tabindex="-1"
+                            aria-disabled="true">Apis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light-navbar font-archivo" href="{{ route('precos') }}">Preços</a>
+                    </li>
+                    <a class="bg-secondary btn-padding text-white  text-decoration-none rounded-4"
+                        href="{{ route('registro') }}" role="button">Registro</a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</aside>
