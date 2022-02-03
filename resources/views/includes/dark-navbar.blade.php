edf7e40c22806bd3bbe8d8b9fbdac6c6ffc86dd7
Fabricio de Freitas <fabricioffa08@gmail.com>
2022-02-03 12:26:03 -0300

    navbar-container-change

--- resources/views/includes/dark-navbar.blade.php
+++ resources/views/includes/dark-navbar.blade.php
@@ -1,6 +1,6 @@
 <aside class="bg-primary fixed-top d-flex border-bottom border-color-navbar-dark">
-    <nav class="navbar navbar-expand-lg navbar-dark col-12 col-lg-10 col-xl-9 bg-primary ms-auto mx-lg-auto px-3 px-lg-5">
-        <div class="container-fluid py-1">
+    <nav class="navbar navbar-expand-lg navbar-dark col-lg-12 bg-primary ms-auto mx-lg-auto px-3 px-lg-5">
+        <div class="container py-1">
             <a class="navbar-brand d-none d-lg-block" href="{{ route('homepage') }}"><img
                     src="{{ asset('assets/imgs/logos/dark-navbar.svg') }}" alt=""></a>
             <button class="navbar-toggler d-block d-lg-none ms-auto ms-lg-0 " type="button" data-bs-toggle="collapse"
@@ -12,7 +12,7 @@
                 <ul class="navbar-nav gap-4 fw-500 align-items-baseline font-space-grotesk">
                     <li class="nav-item dropdown">
                         <a class="nav-link position-relative dropdown-toggle text-white
-                        {{ in_array( request()->path(), ['em-massa', 'para-empresa', 'rotas-sim', 'sem-restricoes']) ? 'checked' : null }}" id="navbarDropdown" role="button"
+                        {{ in_array( request()->path(), ['homepage', 'em-massa', 'para-empresa', 'rotas-sim', 'sem-restricoes']) ? 'checked' : null }}" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             SMS Marketing
                         </a>
