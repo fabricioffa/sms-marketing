f04476d1265de9ec960766f861342aff72fcc176
Fabricio de Freitas <fabricioffa08@gmail.com>
2022-02-04 15:15:02 -0300

    navbar-toggle-icon-color-and-position

--- resources/views/includes/dark-navbar.blade.php
+++ resources/views/includes/dark-navbar.blade.php
@@ -1,18 +1,18 @@
 <aside class="bg-primary fixed-top d-flex border-bottom border-color-navbar-dark">
-    <nav class="navbar navbar-expand-lg navbar-dark col-lg-12 bg-primary ms-auto mx-lg-auto px-3 px-lg-5">
-        <div class="container py-1">
+    <nav class="navbar navbar-expand-lg navbar-dark col-12 bg-primary ms-auto mx-lg-auto ps-3 pe-1 px-lg-5">
+        <div class="container-fluid container-lg py-1">
             <a class="navbar-brand d-none d-lg-block" href="{{ route('homepage') }}"><img
                     src="{{ asset('assets/imgs/logos/dark-navbar.svg') }}" alt=""></a>
-            <button class="navbar-toggler d-block d-lg-none ms-auto ms-lg-0 " type="button" data-bs-toggle="collapse"
+            <button class="navbar-toggler d-block d-lg-none ms-auto ms-lg-0" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon custom-color"><span></span></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav gap-4 fw-500 align-items-baseline font-space-grotesk">
                     <li class="nav-item dropdown">
                         <a class="nav-link position-relative dropdown-toggle text-white
-                        {{ in_array( request()->path(), ['homepage', 'em-massa', 'para-empresa', 'rotas-sim', 'sem-restricoes']) ? 'checked' : null }}" id="navbarDropdown" role="button"
+                        {{ in_array( request()->path(), ['homepage', 'em-massa', 'para-empresa', 'rotas-sim', 'sem-restricoes']) ? 'marked' : null }}" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             SMS Marketing
                         </a>
@@ -28,11 +28,11 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link position-relative text-white {{ request()->path() == 'apis' ? 'checked' : null }}" href="{{ route('apis') }}" tabindex="-1"
+                        <a class="nav-link position-relative text-white {{ request()->path() == 'apis' ? 'marked' : null }}" href="{{ route('apis') }}" tabindex="-1"
                             aria-disabled="true">Apis</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link position-relative text-white font-archivo {{ request()->path() == 'precos' ? 'checked' : null }}" href="{{ route('precos') }}">Preços</a>
+                        <a class="nav-link position-relative text-white font-archivo {{ request()->path() == 'precos' ? 'marked' : null }}" href="{{ route('precos') }}">Preços</a>
                     </li>
                     <a class="bg-secondary btn-padding text-white  text-decoration-none rounded-4"
                         href="{{ route('registro') }}" role="button">Registro</a>
